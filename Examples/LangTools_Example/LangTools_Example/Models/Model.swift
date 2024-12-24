1e0ff6d4d263ad71925bf93abf7ae4d028f92d5b
Reid Chatham <reid.chatham@gmail.com>
2024-12-24 09:59:21 -0800

    filter out non-chat models in ChatSettingsView in example project

--- Examples/LangTools_Example/LangTools_Example/Models/Model.swift
+++ Examples/LangTools_Example/LangTools_Example/Models/Model.swift
@@ -13,12 +13,12 @@ enum Model: Codable, RawRepresentable, Hashable, CaseIterable, Identifiable {
     typealias RawValue = String
     case openAI(OpenAI.Model)
     case anthropic(Anthropic.Model)
-    case xAI(XAIModel)
+    case xAI(XAI.Model)
 
     init?(rawValue: String) {
-        if let modelID = OpenAIModel.ModelID(rawValue: rawValue) { self = .openAI(modelID.openAIModel) }
+        if let model = OpenAI.Model(modelIDString: rawValue) { self = .openAI(model) }
         else if let model = Anthropic.Model(rawValue: rawValue) { self = .anthropic(model) }
-        else if let model = XAIModel(rawValue: rawValue) { self = .xAI(model) }
+        else if let model = XAI.Model(rawValue: rawValue) { self = .xAI(model) }
         else { return nil }
     }
 
@@ -33,6 +33,8 @@ enum Model: Codable, RawRepresentable, Hashable, CaseIterable, Identifiable {
     var id: String { rawValue }
 
     static var allCases: [Model] { OpenAI.Model.allCases.map { .openAI($0) } + Anthropic.Model.allCases.map { .anthropic($0) } + XAI.Model.allCases.map { .xAI($0) } }
+
+    static var chatModels: [Model] { OpenAI.Model.chatModels.map { .openAI($0) } + Anthropic.Model.allCases.map { .anthropic($0) } + XAI.Model.allCases.map { .xAI($0) } }
 
     func hash(into hasher: inout Hasher) {
         hasher.combine(rawValue)
