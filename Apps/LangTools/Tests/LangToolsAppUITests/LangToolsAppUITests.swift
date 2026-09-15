55c8a685145e7dfebb7c2b544e2056e736d06a2e
Reid Chatham <reid.chatham@gmail.com>
2026-09-14 20:19:42 -0700

    test(app): drop vacuous availability guard

--- Apps/LangTools/Tests/LangToolsAppUITests/LangToolsAppUITests.swift
+++ Apps/LangTools/Tests/LangToolsAppUITests/LangToolsAppUITests.swift
@@ -78,11 +78,9 @@ final class LangToolsAppUITests: XCTestCase {
 #if !os(macOS)
     @MainActor
     func testLaunchPerformance() throws {
-        if #available(iOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
         }
     }
 #endif
