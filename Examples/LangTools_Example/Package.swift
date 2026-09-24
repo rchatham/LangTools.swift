a8c7bc8f1536e45892617d8cec19ab4218be7ae2
Reid Chatham <reid.chatham@gmail.com>
2026-09-24 11:48:42 -0700

    chore(chat): drop duplicated ChatUI test dependency

--- Examples/LangTools_Example/Package.swift
+++ Examples/LangTools_Example/Package.swift
@@ -93,7 +93,6 @@ let package = Package(
                 .product(name: "OpenAI", package: "langtools.swift"),
                 .product(name: "Anthropic", package: "langtools.swift"),
                 .product(name: "Ollama", package: "langtools.swift"),
-                .product(name: "ChatUI", package: "ChatUI"),
                 "KeychainAccess",
             ],
             path: "Tests/ChatTests"),
